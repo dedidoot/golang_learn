0ba71114263109d4777277a6d6354c7bd0cb460c
dedi <dedi>
2022-08-29 20:51:53 +0700

    initial

---
+++ day1/number.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("x", 1)
+	fmt.Println("xx", 2)
+	fmt.Println("xxx", 3.5)
+	fmt.Println("okesip")
+
+	_ = fmt.Errorf("")
+}
---
+++ template/oke.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("x", 1)
+	fmt.Println("xx", 2)
+	fmt.Println("xxx", 3.5)
+	fmt.Println("okesipxx")
+}

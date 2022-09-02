dc8195aaec421ed47f3e36fbdbcfc39ff96979eb
dedi <dedi>
2022-09-02 08:06:10 +0700

    comparasion operator

---
+++ day1/comparasion-operator.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	var name1 = "John Doe One"
+	var name2 = "John Doe One"
+
+	isSameName := name1 == name2
+
+	println(isSameName)
+
+	var alphabetA = "A"
+	var alphabetB = "B"
+
+	println(alphabetA < alphabetB)
+
+	println(10 < 5)
+	println(100 > 5)
+	println(10 == 5)
+	println(10 != 5)
+}

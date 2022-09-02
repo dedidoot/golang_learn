d313a538ed4d760b979a4b1784d6defe3c920b1d
dedi <dedi>
2022-09-02 07:49:38 +0700

    type declaration

---
+++ day1/type-declarations.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	type identityNumber string
+	type isMarried bool
+
+	var uniqueNumber identityNumber = "123456789"
+	var married isMarried = false
+
+	println(uniqueNumber)
+	println(married)
+
+}

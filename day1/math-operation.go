34d9cd79ac2e8f0fee92f488d61914400b4a1f66
dedi <dedi>
2022-09-02 07:57:01 +0700

    math operational

---
+++ day1/math-operation.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = 10 + 12
+	println(a)
+
+	var b = a * 12
+	fmt.Println(b)
+
+	b += 1
+	fmt.Println(b)
+
+	b++
+	fmt.Println(b)
+
+	minusThousand := -1000
+	fmt.Println(minusThousand)
+}

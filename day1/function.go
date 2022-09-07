e09c393de3db07f5c6d5f27aa91753cd596a42f9
dedi <dedi>
2022-09-07 21:26:54 +0700

    function

---
+++ day1/function.go
@@ -0,0 +1,15 @@
+package main
+
+func cardanoBlockchain() {
+	println("Cardano Blockchain")
+}
+
+func blockchainFavorite(name string, programming string) {
+	println("Blockchain", name)
+	println("Programming", programming)
+}
+
+func main() {
+	cardanoBlockchain()
+	blockchainFavorite("Ethereum", "Solidity")
+}
